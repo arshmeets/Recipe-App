bb95374c482cec3328845b0114935cb132455f3c
Arshmeet Sodhi <arshmeetsodhi@gmail.com>
2021-02-26 18:59:12 -0500

    - Add the slides/ TabView to the featured cards - Add a little bit of formatting

--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -13,12 +13,46 @@ struct RecipeFeaturedView: View {
     @EnvironmentObject var model:RecipeModel
     
     var body: some View {
-        Text("Hello, World")
+        
+        GeometryReader { geo in
+            
+        TabView{
+            
+            //Loop thorugh each recipe
+            ForEach (0..<model.recipes.count) { index in
+                
+                //Check which recipes are featured to display
+                if model.recipes[index].featured {
+                    
+                    //Recipe card
+                    ZStack {
+                    Rectangle()
+                        .foregroundColor(.white)
+                        VStack(spacing: 0) {
+                            Image("lasagna")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .clipped()
+                            Text("Lasagna")
+                                .padding(5)
+                        }
+                    }
+                    .frame(width: geo.size.width-40, height: geo.size.height-100, alignment: .center)
+                    .cornerRadius(15)
+                    .shadow(radius: 20)
+                }
+                
+            }
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+        }
     }
 }
 
 struct RecipeFeaturedView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeFeaturedView()
+            .environmentObject(RecipeModel())
     }
 }
