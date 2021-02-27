2072408217edabd4e819987df005a2adfff2ccd0
Arshmeet Sodhi <arshmeetsodhi@gmail.com>
2021-02-27 12:55:43 -0500

    - Add some spacing and change the shadows - Add titles and descriptions - Add images and names accordingly

--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -14,38 +14,56 @@ struct RecipeFeaturedView: View {
     
     var body: some View {
         
+        VStack(alignment:.leading, spacing: 0) {
+            Text("Featured Recipes")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.leading)
+                .padding(.top, 40)
+            
         GeometryReader { geo in
             
-        TabView{
-            
-            //Loop thorugh each recipe
-            ForEach (0..<model.recipes.count) { index in
+            TabView{
                 
-                //Check which recipes are featured to display
-                if model.recipes[index].featured {
+                //Loop thorugh each recipe
+                ForEach (0..<model.recipes.count) { index in
                     
-                    //Recipe card
-                    ZStack {
-                    Rectangle()
-                        .foregroundColor(.white)
-                        VStack(spacing: 0) {
-                            Image("lasagna")
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .clipped()
-                            Text("Lasagna")
-                                .padding(5)
+                    //Check which recipes are featured to display
+                    if model.recipes[index].featured {
+                        
+                        //Recipe card
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.white)
+                            VStack(spacing: 0) {
+                                Image(model.recipes[index].image)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .clipped()
+                                Text(model.recipes[index].name)
+                                    .padding(5)
+                            }
                         }
+                        .frame(width: geo.size.width-40, height: geo.size.height-100, alignment: .center)
+                        .cornerRadius(15)
+                        .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
                     }
-                    .frame(width: geo.size.width-40, height: geo.size.height-100, alignment: .center)
-                    .cornerRadius(15)
-                    .shadow(radius: 20)
+                    
                 }
-                
             }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
         }
-        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
-        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            
+            VStack(alignment:.leading, spacing: 10) {
+                Text("Preparation Time:")
+                    .font(.headline)
+                Text("1 Hour")
+                Text("Highlights:")
+                    .font(.headline)
+                Text("Healthy, Hearty")
+            }
+            .padding([.leading, .bottom])
         }
     }
 }
