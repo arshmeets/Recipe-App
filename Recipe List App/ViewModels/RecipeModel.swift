fd1a24d3696dcbbc0f39c399b00a9b88bcaf01e8
Arshmeet Sodhi <arshmeetsodhi@gmail.com>
2021-03-01 18:13:41 -0500

    - Add the greatest common divisor logic and implement an instance in the view model - Add denominators and numerators - Add the logic for spacing and the suffixes in the view model

--- Recipe List App/ViewModels/RecipeModel.swift
+++ Recipe List App/ViewModels/RecipeModel.swift
@@ -20,16 +20,73 @@ class RecipeModel: ObservableObject {
     
     static func getPortion(ingredient:Ingredients, recipeServings:Int, targetServings:Int) -> String {
         
-        // Get a single serving size by multiplying denominator by the recipe servings
+        var portion = ""
+        var numerator = ingredient.num ?? 1
+        var denominator = ingredient.denom ?? 1
+        var wholePortions = 0
         
-        // Get target portion by multiplying numerator by target servings
+        if ingredient.num != nil {
+            
+            // Get a single serving size by multiplying denominator by the recipe servings
+            denominator *= recipeServings
+            
+            // Get target portion by multiplying numerator by target servings
+            numerator *= targetServings
+            
+            // Reduce fraction by the greatest common divisor
+            let divisor = Rational.greatestCommonDivisor(numerator, denominator)
+            numerator /= divisor
+            denominator /= divisor
+            
+            // Get the whole portion if numerator > denominator
+            if numerator >= denominator {
+                
+                // Calculated whole portion
+                wholePortions = numerator/denominator
+                
+                // Calculate the remainder
+                numerator = numerator % denominator
+                
+                // Assign to portion string
+                portion += String(wholePortions)
+            }
+            
+            // Express the remainder as a fraction
+            if numerator > 0 {
+                
+                //Assign remainder as fraction to the portion string
+                portion += wholePortions > 0 ? " " : ""
+                
+                portion += "\(numerator)/\(denominator)"
+            }
+            
+        }
         
-        // Reduce fraction by the greatest common divisor
+        if var unit = ingredient.unit {
+            
+            //If we need to pluralize
+            if wholePortions > 1 {
+                //Calculate appropriate suffix
+                if unit.suffix(2) == "ch" {
+                    unit += "es"
+                }
+                else if unit.suffix(1) == "f" {
+                    unit = String(unit.dropLast())
+                    unit += "ves"
+                }
+                else {
+                    unit += "s"
+                }
+            }
+            
+            
+            // Add a space after the number of items there
+            portion += ingredient.num == nil && ingredient.denom == nil ? "" : " "
+            
+            return portion + unit
+            
+        }
         
-        // Get the whole portion if numerator > denominator
-        
-        // Express the remainder as a fraction
-        
-        return String(targetServings)
+        return portion
     }
 }
