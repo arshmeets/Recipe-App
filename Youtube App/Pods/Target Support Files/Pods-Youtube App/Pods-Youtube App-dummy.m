9c8417c89622241f872b368832dece9c3fc340cb
Arshmeet Sodhi <arshmeetsodhi@gmail.com>
2021-03-12 20:53:05 -0500

    - The whole calculator app - Fix the decimal point problem ( decimal point ads another 0 after instead)

---
+++ Youtube App/Pods/Target Support Files/Pods-Youtube App/Pods-Youtube App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Youtube_App : NSObject
+@end
+@implementation PodsDummy_Pods_Youtube_App
+@end
---
+++ Youtube App/Youtube App/Youtube_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Youtube_AppApp.swift
+//  Youtube App
+//
+//  Created by Arshmeet Sodhi on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Youtube_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
